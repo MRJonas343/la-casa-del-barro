3b4cd14b36e47d3c8409e34b4d42d7acb860fb37
MRJonas343 <angeljonasrosales@gmail.com>
2024-10-04 17:47:03 -0500

    Refactor [tag] page to streamline component structure and remove unused data fetching logic for enhanced clarity and maintainability

--- src/app/(latest-forms)/[tag]/page.tsx
+++ src/app/(latest-forms)/[tag]/page.tsx
@@ -1,35 +1,6 @@
-export const revalidate = 60;
+import type { PageProps } from "./interfaces/pageProps";
 
-const Page = async ({ params }: { params: { tag: string } }) => {
-	const data = await fetchData(params.tag);
-
+const page = ({ params }: PageProps) => {
 	return <div>{params.tag} forms</div>;
 };
-
-export default Page;
-
-async function fetchData(tag: string) {
-	//TODO: Get the data form the DB
-	return { tag };
-}
-
-export async function generateStaticParams() {
-	const tags = [
-		"education",
-		"technology",
-		"environment",
-		"science",
-		"literature",
-		"health",
-		"business",
-		"finance",
-		"maths",
-		"history",
-		"animals",
-		"sports",
-	];
-
-	return tags.map((tag) => ({
-		tag,
-	}));
-}
+export default page;
