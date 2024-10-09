583a723f1f19e1ca2f212a504152c53da5562f72
MRJonas343 <angeljonasrosales@gmail.com>
2024-10-09 18:02:33 -0500

    Refactor drizzle config to include new schema files, streamline database token handling, and remove redundant entries

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,23 +4,21 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env" });
 
 export default defineConfig({
+	dialect: "sqlite",
 	schema: [
-		"./src/db/schemas/answerSchema.ts",
-		"./src/db/schemas/commentSchema.ts",
-		"./src/db/schemas/filledFormSchema.ts",
-		"./src/db/schemas/formSchema.ts",
-		"./src/db/schemas/likeSchema.ts",
-		"./src/db/schemas/questionSchema.ts",
-		"./src/db/schemas/userSchema.ts",
 		"./src/db/schemas/accountsSchema.ts",
 		"./src/db/schemas/authenticatorsSchema.ts",
+		"./src/db/schemas/answersSchema.ts",
+		"./src/db/schemas/commentsSchema.ts",
+		"./src/db/schemas/filledFormSchema.ts",
+		"./src/db/schemas/formsSchema.ts",
+		"./src/db/schemas/likesSchema.ts",
+		"./src/db/schemas/questionsSchema.ts",
 		"./src/db/schemas/sessionsSchema.ts",
+		"./src/db/schemas/usersSchema.ts",
 	],
-	out: "./migrations",
-	dialect: "sqlite",
-	driver: "turso",
 	dbCredentials: {
 		url: process.env.TURSO_CONNECTION_URL ?? "",
-		authToken: process.env.TURSO_AUTH_TOKEN ?? "",
+		token: process.env.TURSO_AUTH_TOKEN ?? "",
 	},
 });
