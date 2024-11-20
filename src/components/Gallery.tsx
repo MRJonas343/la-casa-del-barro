bd71d01b632ead673b1fd84ac472878341944eaf
MRJonas343 <angeljonasrosales@gmail.com>
2024-11-19 23:12:22 -0500

    refactor: update Gallery component to use @wowblvck/react-responsive-masonry and optimize image rendering with NextUI's Image component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Masonry from "react-responsive-masonry";
-import { ResponsiveMasonry } from "react-responsive-masonry";
-
+import MasonryFC from "@wowblvck/react-responsive-masonry";
+import { Image } from "@nextui-org/image";
 interface GalleryProps {
 	images: Images[];
 }
@@ -15,18 +14,17 @@ interface Images {
 
 const Gallery = ({ images }: GalleryProps) => {
 	return (
-		<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-			<Masonry gutter="16px">
-				{images.map((image) => (
-					<img
-						key={image.id}
-						src={image.src}
-						alt={image.alt}
-						className="rounded-xl"
-					/>
-				))}
-			</Masonry>
-		</ResponsiveMasonry>
+		<MasonryFC gutter="16px">
+			{images.map((image) => (
+				<Image
+					key={image.id}
+					src={image.src}
+					alt={image.alt}
+					radius="lg"
+					isZoomed
+				/>
+			))}
+		</MasonryFC>
 	);
 };
 export default Gallery;
